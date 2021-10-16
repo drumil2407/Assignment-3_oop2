07aaefc0f8c5598f5a1218c20ac3e4dc8b590abf
drumil2407 <78135386+drumil2407@users.noreply.github.com>
2021-10-16 03:19:16 -0400

    Assignment-3
    Inheritance
    Polymorphism
    Abstract classes

---
+++ Assignment-3_oop2/Birds.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Assignment_3_oop2.Eggs;
+
+namespace Assignment_3_oop2.Birds
+{
+    class Bird
+    {
+		public static Random Rand = new Random(1);
+		public virtual Egg[] LayEggs(int numEggs)
+		{
+			Console.Error.WriteLine("Bird.LayEggs should not be called!");
+			return new Egg[0];
+		}
+	}
+	class Chicken : Bird
+	{
+		// TODO: Implement this class
+	}
+
+	class Ostrich : Bird
+	{
+		// TODO: Implement this class
+	}
+}
+
