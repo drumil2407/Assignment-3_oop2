2d16e7faf491808e65adbe600b5dc50dbf01739c
drumil2407 <78135386+drumil2407@users.noreply.github.com>
2021-10-18 23:10:33 -0400

    2 commit

--- Assignment-3_oop2/Birds.cs
+++ Assignment-3_oop2/Birds.cs
@@ -13,51 +13,67 @@ namespace Assignment_3_oop2.Birds
 			return new Egg[0];
 			
 		}
+
+		
 		
 	}
 	class Chicken : Bird
 	{
-		// TODO: Implement this class
-
-		Egg E = new Egg(4,Egg.Colors.brown);
-		
-		
-
+		BrokenEgg E = new BrokenEgg(0.2,Egg.Colors.brown);
 		public override Egg[] LayEggs(int numEggs)
 		{
-
-			for (int i = 0; i < 5; i++)
+            Console.WriteLine(E);
+			Egg[] chickenEgg = new Egg[numEggs];
+			int i=0;
+			while(i < numEggs)
 			{
-				Console.WriteLine(E);
-				
-
+				if (i < numEggs)
+				{
+					GetEgg(2,4,0.2,Egg.Colors.brown);
+				}
+				chickenEgg[i] = new Egg(Rand.Next(2,4), Egg.Colors.brown);
+				i++;
 			}
-			return new Egg[0];
+			return chickenEgg;
+		}
+		protected Egg GetEgg(double minSize, double maxSize, double brokenChance, Egg.Colors color) 
+		{
+			//Egg brokenegg = new BrokenEgg(Rand.Next(2,4),Egg.Colors.brown);
+			
+			//Egg brokenchickenegg = new Egg(2,4,0.2,Egg.Colors.brown);
+			return GetEgg(2,4,0.2,Egg.Colors.brown);
+		
 		}
 
-		
-		
         
-
 	}
 
 	class Ostrich : Bird
 	{
-		// TODO: Implement this class
-
-		Egg E1 = new Egg(15, Egg.Colors.speckled);
+		//Egg E1 = new Egg(15, Egg.Colors.speckled);
+		BrokenEgg E1 = new BrokenEgg(0.4, Egg.Colors.speckled);
 
 
 		public override Egg[] LayEggs(int numEggs)
 		{
-
-			for (int i = 0; i < 5; i++)
-			{
-				Console.WriteLine(E1);
-
+            Console.WriteLine(E1);
+			Egg[] ostrichEgg = new Egg[numEggs];
+			int i = 0;
+			while (i < numEggs)
+			{	
+				ostrichEgg[i] = new Egg(Rand.Next(10, 15), Egg.Colors.speckled);
+				i++;
 			}
-			return new Egg[0];
+			return ostrichEgg;
 		}
+
+		protected Egg GetEgg(double minSize, double maxSize, double brokenChance, Egg.Colors color)
+		{
+
+			return GetEgg(2, 4, 0.25, Egg.Colors.speckled);
+
+		}
+
 	}
 }
 
--- Assignment-3_oop2/Eggs.cs
+++ Assignment-3_oop2/Eggs.cs
@@ -10,33 +10,35 @@ namespace Assignment_3_oop2.Eggs
 				speckled,
 				brown
 			}
-			public static Random Rand = new Random(1);
-		//public  double Size { get; private set; }
-		   public double Size
-		   {
-				get { return Rand.Next(2, 15);}
-				private set { }
-
-		   }
+		   
+		   public double Size {get;private set;}
 
 		    public Colors Color { get; private set; }
 			public Egg(double size, Colors color)
 			{
 				Size = size;
 				Color = color;
-			}
-			
-		    
+			} 
 			public override string ToString()
 			{
 				return $"A {Size:0.0}cm {Color} egg";
 			}
 			
 		}
-		/*
+		
 		class BrokenEgg : Egg
 		{
-			// TODO: Implement for Task 2
-		}
-		*/
+			public BrokenEgg(double size, Colors color) : base(size, color)
+			{
+
+			}
+
+		   public override string ToString()
+		   {
+				
+				return $"A {Size:0.0}cm {Color} egg that is broken";
+		   }
+
+	    }
+		
 }
